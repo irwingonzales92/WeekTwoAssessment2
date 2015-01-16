dc5a11dacac017fd90e58d09092d2f44cf24a4d2
irwingonzales92 <irwin@newdesto.com>
2015-01-16 09:29:13 -0800

    Initial Commit

---
+++ Week 2 Assessment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week 2 Assessment
+//
+//  Created by Irwin Gonzales on 1/16/15.
+//  Copyright (c) 2015 Irwin Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
